4db710a1192b96e1ca022cb29e5071706189267f
joão vinícius <j.vin7cius@gmail.com>
2023-03-07 22:58:17 -0300

    aulas 1-3

---
+++ aulas/aula003/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Caractere de escape \n salta uma linha.
+
+    // Função de saída
+    // O printf() retorna um número
+    printf("\nValor retornado pelo printf: (quantidade de caracteres) %d", printf("Ola!"));
+
+    // %d é uma máscara para números inteiros
+    // system("pause") só funciona no Windows.
+
+    return 0;
+}
